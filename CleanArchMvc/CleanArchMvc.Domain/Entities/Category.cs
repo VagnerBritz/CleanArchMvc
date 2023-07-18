cba46a77abbf55260a74063c2a4e290aa061d459
VagnerBritz <vagnerbritz@gmail.com>
2023-07-18 11:02:37 -0300

    Create product and Category classes.
    Adds validation to category class with a validation class DomainExceptionValidation

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        // vou precisar desse construtor quando for povoar a tabela.
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id Values");
+            Id = id;
+            ValidateDomain(name);
+        }
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CleanArchMvc
 Aplicação ASP .NET Core MVC com produtos e categorias que podem ser usados para catálogo de vendas.  Projeto faz parte do curso de Clean Architecture. Prof  Macoratti.
+## Camada Domain - obj e recursos usados:
+
+Criação de domínio não anêmico, isolando o domínio do mundo externo de forma que ele seja independente
+de todas as outras camadas do projeto e garantir que as entidades sejam sempre válidas.
+
+### Recursos:
+- Classe `sealed`;
+- Setters `private`;
+- Validação do modelo;
+- Interfaces para o repositório;
+- Testes unitários para o projeto `Domain`;
