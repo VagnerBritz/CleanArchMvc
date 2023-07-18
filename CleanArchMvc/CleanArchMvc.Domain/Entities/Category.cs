4649dcaa108fb770bfc1012876a2a4d08bbb2b68
VagnerBritz <vagnerbritz@gmail.com>
2023-07-18 11:31:26 -0300

    Feat - Implements product class validations.

--- CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -7,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public sealed class Category
+    public sealed class Category : Entity
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
         public ICollection<Product> Products { get; private set; }
         public Category(string name)
         {
@@ -23,9 +21,13 @@ namespace CleanArchMvc.Domain.Entities
             Id = id;
             ValidateDomain(name);
         }
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
         private void ValidateDomain(string name)
         {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name is required");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
             DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
 
             Name = name;
