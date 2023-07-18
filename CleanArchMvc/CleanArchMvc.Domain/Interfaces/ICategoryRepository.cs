f3db2f824c12dbbc539360ab5ca679e01faf84ba
VagnerBritz <vagnerbritz@gmail.com>
2023-07-18 12:12:17 -0300

    Feat - Create interface for Domain project

--- CleanArchMvc/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace CleanArchMvc.Domain.Interfaces
 {
     internal interface ICategoryRepository
     {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int? id);
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
     }
 }
