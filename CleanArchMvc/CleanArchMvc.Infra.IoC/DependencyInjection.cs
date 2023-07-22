6d7ef7c69f1f5a39a133d456be0b14bcac412aa6
VagnerBritz <vagnerbritz@gmail.com>
2023-07-22 12:08:10 -0300

    Feat - Adding the Identity services and registering the services in the Infra.IoC project

--- CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,13 +1,15 @@
 ﻿using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using AutoMapper;
 
 namespace CleanArchMvc.Infra.IoC
 {
@@ -25,8 +27,14 @@ namespace CleanArchMvc.Infra.IoC
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>() // permite realizar a persistência no SQL server dos users e roles
+                .AddDefaultTokenProviders(); // para a geração de token de autenticação de dois fatores.
+            services.ConfigureApplicationCookie(options =>
+                options.AccessDeniedPath = "/Account/Login"); //quando o user tentar acessar algo que ele não tem acesso será redirecionado para area de login
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
             return services;
         }
     }
 }
-  
